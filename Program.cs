f634cbe1084af31f038c7aeadb573a5454444208
BENJAMÍN ALFONSO IGNACIO CONTRERAS ORTEGA <b.contreras45@alumnos.santotomas.cl>
2021-04-19 14:22:32 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certamen.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese monto solicitado");
+            double monto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese cantidad de cuotas");
+            int cuotas = int.Parse(Console.ReadLine());
+
+            Consola c1 = new Consola();
+            c1.monto_solicitado = monto;
+            c1.cuotas = cuotas;
+
+            c1.calcular_todo();
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
